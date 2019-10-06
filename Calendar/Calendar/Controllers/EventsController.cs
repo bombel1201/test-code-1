5e62093ffcb18a6913e43ac9b5bdf0c1a64df3ef
Radoslaw Dabrowicz <radoslaw.dabrowicz@pl.abb.com>
2019-10-06 12:24:50 +0200

    ValuesController changed to EventsController

---
+++ Calendar/Calendar/Controllers/EventsController.cs
@@ -0,0 +1,50 @@
+﻿using Calendar.Models;
+using Calendar.Models.Repetitions;
+using Calendar.Providers;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Calendar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly IEventsFactory eventsFactory;
+        private readonly IRepetitionFactory repetitionFactory;
+
+        public EventsController(IEventsFactory eventsFactory,
+            IRepetitionFactory repetitionFactory)
+        {
+            this.repetitionFactory = repetitionFactory;
+            this.eventsFactory = eventsFactory;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public ActionResult<Event> CreateSingleEvent()
+        {
+            return eventsFactory.CreateSingleEvent();
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public ActionResult<Repetition> CreateRepetition([FromQuery] RepetitionPeriod period)
+        {
+            return repetitionFactory.Create(period);
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public void SaveEvent([FromBody] Event value)
+        {
+
+        }
+    }
+}
