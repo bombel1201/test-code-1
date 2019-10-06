3f1d29def08ab17e34a427af892fd6a7ba9e26e7
Radoslaw Dabrowicz <radoslaw.dabrowicz@pl.abb.com>
2019-10-06 12:25:00 +0200

    Test project added

---
+++ Calendar/CalendarTests/Providers/EventsFactoryTests.cs
@@ -0,0 +1,26 @@
+using Calendar.Models;
+using Calendar.Models.Repetitions;
+using Calendar.Providers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace CalendarTests
+{
+    [TestClass]
+    public class EventsFactoryTests
+    {
+        [TestMethod]
+        public void Call_CreateSingleEvent_WithoutParameter_ReturnsProperInstance()
+        {
+            var repetition = new Mock<Repetition>((uint)1, RepetitionPeriod.None);
+            var repetitionFactory = Mock.Of<IRepetitionFactory>(
+                x => x.Create(RepetitionPeriod.None) == repetition.Object);
+            var factory = new EventsFactory(repetitionFactory);
+
+            var actual = factory.CreateSingleEvent();
+
+            Assert.IsInstanceOfType(actual, typeof(Event));
+            Assert.AreEqual(repetition.Object, actual.Repetition);
+        }
+    }
+}
---
+++ Calendar/CalendarTests/Providers/WeekDaysFactoryTests.cs
@@ -0,0 +1,43 @@
+﻿using Calendar.Providers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace CalendarTests.Providers
+{
+    [TestClass]
+    public class WeekDaysFactoryTests
+    {
+        [TestMethod]
+        public void CreateInitialListOfWeekDays_TodaySelectedIsEqualToTrue_ReturnCollectionContainsAllDaysWithCurrentDaySelected()
+        {
+            var factory = new WeekDaysFactory();
+            var actual = factory.CreateInitialListOfWeekDays(true);
+            var expectedDays = Enum.GetValues(typeof(DayOfWeek)).OfType<DayOfWeek>().ToList();
+            CollectionAssert.AreEqual(expectedDays, actual.Select(x => x.Day).ToList());
+
+            var expectedSelectedDay = DateTime.Today.DayOfWeek;
+            foreach (var day in actual)
+            {
+                if (day.Day == expectedSelectedDay)
+                {
+                    Assert.IsTrue(day.Selected);
+                }
+                else
+                {
+                    Assert.IsFalse(day.Selected);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void CreateInitialListOfWeekDays_TodaySelectedIsEqualToFalse_ReturnCollectionContainsAllDaysAllValuesAreUnselected()
+        {
+            var factory = new WeekDaysFactory();
+            var actual = factory.CreateInitialListOfWeekDays(false);
+            var expectedDays = Enum.GetValues(typeof(DayOfWeek)).OfType<DayOfWeek>().ToList();
+            CollectionAssert.AreEqual(expectedDays, actual.Select(x => x.Day).ToList());
+            Assert.IsFalse(actual.Any(x => x.Selected));
+        }
+    }
+}
