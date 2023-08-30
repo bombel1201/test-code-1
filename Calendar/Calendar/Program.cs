45a6201a38bf09b029821c9dd3b0f3f130e6ba37
Radoslaw Dabrowicz <radoslaw.dabrowicz@hitachienergy.com>
2023-08-30 19:00:47 +0200

    Comment

--- Calendar/Calendar/Program.cs
+++ Calendar/Calendar/Program.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-
-namespace Calendar
+﻿namespace Calendar
 {
     public class Program
     {
@@ -17,8 +7,11 @@ namespace Calendar
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        //aa
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+        }
     }
 }
